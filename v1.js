6841a744f87471042c8492fcecb5fc98f38f9ba0
shivansh <shivanshrao464@gmail.com>
2020-02-10 17:30:45 +0000

    global mongo

--- v1.js
+++ v1.js
@@ -18,7 +18,9 @@ app.use(methodOverride("_method"));
 app.set("view engine", "ejs");
 
 app.use(bodyparser.urlencoded({extended: true}));
-mongoose.connect("mongodb://localhost:27017/yelp_camp", {useUnifiedTopology: true});
+//mongoose.connect("mongodb://localhost:27017/yelp_camp", {useUnifiedTopology: true});
+mongoose.connect("mongodb+srv://shivansh:shivansh@cluster0-fpqlm.mongodb.net/test?retryWrites=true&w=majority", {useUnifiedTopology: true});
+
 app.use(express.static("public"));
 //seedDB();
 
