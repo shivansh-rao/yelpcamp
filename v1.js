0e97333203f5ffe3e30f26150662bbd6d3231e41
shivansh <shivanshrao464@gmail.com>
2020-02-10 18:50:00 +0000

    ll

--- v1.js
+++ v1.js
@@ -234,6 +234,6 @@ function isAllowed(req,res,next){
 		res.redirect("back");
 }
 
-app.listen(process.env.port,() =>{
+app.listen((process.env.PORT || 3000),() =>{
 	console.log("The Yelpcamp app has started");
 });
